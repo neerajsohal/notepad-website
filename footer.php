43f33efa3e9638d6f191f4146b2a1e6f43d62bac
neerajsohal <hello@neerajkumar.name>
2019-07-27 17:42:48 +0530

    added privacy policy

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="container-fluid prepend-top">
+    <div class="row-fluid">
+        <div class="span12 muted">
+            <hr />
+            <p><small>Copyright &copy; 2012 - <?php echo date('Y'); ?> <a href="http://neerajkumar.name">Neeraj Kumar</a>. <a href="http://lab.neerajkumar.name/notepad/privacy.php">Privacy Policy</a> . <a target="_blank" href="https://play.google.com/store/apps/details?id=name.neerajkumar.mobile.android.notepad">Download</a></small></p>
+            <p><small><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Notapad</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="https://play.google.com/store/apps/details?id=name.neerajkumar.mobile.android.notepad" property="cc:attributionName" rel="cc:attributionURL">Neeraj Kumar</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US">Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License</a>.</small></p>
+        </div>
+    </div>
+</div>
---
+++ privacy.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Notepad</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./statics/css/bootstrap.min.css" rel="stylesheet">
+    <link href="./statics/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="./statics/fancybox/jquery.fancybox.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href="./statics/css/site.css" rel="stylesheet">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+    <!-- meta tags //-->
+
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Notepad - Notes taking app for Android">
+    <meta property="og:site_name" content="Notepad">
+    <meta property="og:description" content="Notepad is a simple, beautiful yet powerful notes taking app for android.">
+    <meta property="og:url" content="http://lab.neerajkumar.name/notepad/">
+    <meta property="og:determiner" content="the">
+    <meta property="og:image" content="https://lh5.ggpht.com/iu8u1RLprmQOIsRrLgnBUqhOJvxArb5shtdjatnVTYzYT36nyIL0kfVGBG333PRdJE0=w300-rw">
+    <meta property="fb:admins" content="609558078">
+
+</head>
+
+<body>
+    <div id="fb-root"></div>
+    <script>
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <div class="main">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12 centerText">
+                    <p><img style="height: 100px" src="https://lh5.ggpht.com/iu8u1RLprmQOIsRrLgnBUqhOJvxArb5shtdjatnVTYzYT36nyIL0kfVGBG333PRdJE0=w300-rw"/ ></p>
+                    <h1>NOTEPAD</h1>
+                    <p class="muted">Notes taking app for Android!</p>
+                    <hr />
+                    <h2>Notepad is a simple, beautiful yet powerful notes taking app for android.</h2>
+                    <p>Unlike other notes taking apps for Android, it's based on minimal design, elegant typography</p>
+                    <br/>
+                    <p>
+                        <a href="https://play.google.com/store/apps/details?id=name.neerajkumar.mobile.android.notepad">
+                            <img alt="Get it on Google Play"
+                                src="https://developer.android.com/images/brand/en_generic_rgb_wo_60.png" />
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid prepend-top">
+            <div class="row-fluid ">
+                <div class="span12">
+                    <div id="privacy_content">
+                        <h2>Privacy Policy</h2>
+                        <p>
+                            Neeraj Kumar built the Notepad app as
+                            an Ad Supported app. This SERVICE is provided by
+                            Neeraj Kumar at no cost and is intended for
+                            use as is.
+                        </p>
+                        <p>
+                            This page is used to inform visitors regarding
+                            my policies with the collection, use, and
+                            disclosure of Personal Information if anyone decided to use
+                            my Service.
+                        </p>
+                        <p>
+                            If you choose to use my Service, then you agree
+                            to the collection and use of information in relation to this
+                            policy. The Personal Information that I collect is
+                            used for providing and improving the Service.
+                            I will not use or share your
+                            information with anyone except as described in this Privacy
+                            Policy.
+                        </p>
+                        <p>
+                            The terms used in this Privacy Policy have the same meanings
+                            as in our Terms and Conditions, which is accessible at
+                            Notepad unless otherwise defined in this Privacy
+                            Policy.
+                        </p>
+                        <p><strong>Information Collection and Use</strong></p>
+                        <p>
+                            For a better experience, while using our Service,
+                            I may require you to provide us with certain
+                            personally identifiable information. The
+                            information that I request will be
+                            retained on your device and is not collected by me in any way.
+                        </p>
+                        <p>
+                            The app does use third party services that may collect
+                            information used to identify you.
+                        </p>
+                        <div>
+                            <p>
+                                Link to privacy policy of third party service providers
+                                used by the app
+                            </p>
+                            <ul>
+                                <!---->
+                                <li><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank">AdMob</a></li>
+                                <li><a href="https://firebase.google.com/policies/analytics" target="_blank">Firebase Analytics</a></li>
+                                <!---->
+                                <!---->
+                                <!---->
+                                <!---->
+                                <!---->
+                                <!---->
+                                <!---->
+                                <!---->
+                            </ul>
+                        </div>
+                        <p><strong>Log Data</strong></p>
+                        <p>
+                            I want to inform you that whenever
+                            you use my Service, in a case of an error in the
+                            app I collect data and information (through third
+                            party products) on your phone called Log Data. This Log Data
+                            may include information such as your device Internet
+                            Protocol (“IP”) address, device name, operating system
+                            version, the configuration of the app when utilizing
+                            my Service, the time and date of your use of the
+                            Service, and other statistics.
+                        </p>
+                        <p><strong>Cookies</strong></p>
+                        <p>
+                            Cookies are files with a small amount of data that are
+                            commonly used as anonymous unique identifiers. These are
+                            sent to your browser from the websites that you visit and
+                            are stored on your device's internal memory.
+                        </p>
+                        <p>
+                            This Service does not use these “cookies” explicitly.
+                            However, the app may use third party code and libraries that
+                            use “cookies” to collect information and improve their
+                            services. You have the option to either accept or refuse
+                            these cookies and know when a cookie is being sent to your
+                            device. If you choose to refuse our cookies, you may not be
+                            able to use some portions of this Service.
+                        </p>
+                        <p><strong>Service Providers</strong></p>
+                        <p>
+                            I may employ third-party companies
+                            and individuals due to the following reasons:
+                        </p>
+                        <ul>
+                            <li>To facilitate our Service;</li>
+                            <li>To provide the Service on our behalf;</li>
+                            <li>To perform Service-related services; or</li>
+                            <li>To assist us in analyzing how our Service is used.</li>
+                        </ul>
+                        <p>
+                            I want to inform users of this
+                            Service that these third parties have access to your
+                            Personal Information. The reason is to perform the tasks
+                            assigned to them on our behalf. However, they are obligated
+                            not to disclose or use the information for any other
+                            purpose.
+                        </p>
+                        <p><strong>Security</strong></p>
+                        <p>
+                            I value your trust in providing us
+                            your Personal Information, thus we are striving to use
+                            commercially acceptable means of protecting it. But remember
+                            that no method of transmission over the internet, or method
+                            of electronic storage is 100% secure and reliable, and
+                            I cannot guarantee its absolute security.
+                        </p>
+                        <p><strong>Links to Other Sites</strong></p>
+                        <p>
+                            This Service may contain links to other sites. If you click
+                            on a third-party link, you will be directed to that site.
+                            Note that these external sites are not operated by
+                            me. Therefore, I strongly advise you to
+                            review the Privacy Policy of these websites.
+                            I have no control over and assume no
+                            responsibility for the content, privacy policies, or
+                            practices of any third-party sites or services.
+                        </p>
+                        <p><strong>Children’s Privacy</strong></p>
+                        <p>
+                            These Services do not address anyone under the age of 13.
+                            I do not knowingly collect personally
+                            identifiable information from children under 13. In the case
+                            I discover that a child under 13 has provided
+                            me with personal information,
+                            I immediately delete this from our servers. If you
+                            are a parent or guardian and you are aware that your child
+                            has provided us with personal information, please contact
+                            me so that I will be able to do
+                            necessary actions.
+                        </p>
+                        <p><strong>Changes to This Privacy Policy</strong></p>
+                        <p>
+                            I may update our Privacy Policy from
+                            time to time. Thus, you are advised to review this page
+                            periodically for any changes. I will
+                            notify you of any changes by posting the new Privacy Policy
+                            on this page. These changes are effective immediately after
+                            they are posted on this page.
+                        </p>
+                        <p><strong>Contact Us</strong></p>
+                        <p>
+                            If you have any questions or suggestions about
+                            my Privacy Policy, do not hesitate to contact
+                            me at hello@neerajkumar.name.
+                        </p>
+                        <p>
+                            This privacy policy page was created at
+                            <a href="https://privacypolicytemplate.net" target="_blank">privacypolicytemplate.net</a>
+                            and modified/generated by
+                            <a href="https://app-privacy-policy-generator.firebaseapp.com/" target="_blank">App Privacy Policy Generator</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php include_once "footer.php" ?>
+
+    </div>
+
+    <br />
+    </br>
+
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="./statics/js/bootstrap.min.js"></script>
+    <script src="./statics/fancybox/jquery.fancybox.js"></script>
+    <script src="./statics/js/site.js"></script>
+    <script>
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-43788904-1', 'neerajkumar.name');
+        ga('send', 'pageview');
+    </script>
+</body>
+
+</html>
